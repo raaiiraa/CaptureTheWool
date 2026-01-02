d0163695dd0dccea5e782560bc87692052885726
Rai <lynx7478@gmail.com>
2026-01-02 14:57:59 +0100

    Update announcement bar

--- src/com/gmail/lynx7478/ctw/announcementbar/versions/v1_21_R1/Bar.java
+++ src/com/gmail/lynx7478/ctw/announcementbar/versions/v1_21_R1/Bar.java
@@ -3,13 +3,16 @@ package com.gmail.lynx7478.ctw.announcementbar.versions.v1_21_R1;
 
 import org.bukkit.entity.Player;
 
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+
 public class Bar implements com.gmail.lynx7478.ctw.announcementbar.Bar
 {
 
 	@Override
 	public void sendToPlayer(Player player, String message, float percentOfTotal) 
 	{
-		
+		player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(cleanMessage(message)));
 	}
     /* @Override
     public void sendToPlayer(final Player player, final String message, final float percentOfTotal)
@@ -17,7 +20,7 @@ public class Bar implements com.gmail.lynx7478.ctw.announcementbar.Bar
         Component actionComponent = Component.Serializer.fromJson("{\"text\":\"" + cleanMessage(message) + "\"}", null);
         ClientboundSystemChatPacket actionPacket = new ClientboundSystemChatPacket(actionComponent, true);
         ((CraftPlayer) player).getHandle().connection.sendPacket(actionPacket);
-    }
+    }*/
 
     private static String cleanMessage(String message)
     {
@@ -25,7 +28,5 @@ public class Bar implements com.gmail.lynx7478.ctw.announcementbar.Bar
             message = message.substring(0, 63);
 
         return message;
-    } */
-	
-	//TODO: Fix
+    }
 }
